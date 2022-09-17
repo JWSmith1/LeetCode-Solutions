17eed8f8b81fed99c3cccba8b22167156d9685b9
Jacob Smith <74027890+JWSmith1@users.noreply.github.com>
2022-09-17 11:11:17 -0400

    Time: 2 ms (47.20%), Space: 47.6 MB (65.20%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int slow = 0;
+        int fast = 1;
+        
+        while (fast < nums.length){
+            if (nums[fast] != nums[slow]){
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+
+        }
+        
+        return slow + 1;
+    }
+}
