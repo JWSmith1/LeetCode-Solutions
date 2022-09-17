d68a241b39222fc0a9f59817f5f5a52eba0083da
Jacob Smith <74027890+JWSmith1@users.noreply.github.com>
2022-09-17 11:19:15 -0400

    Time: 4 ms (59.12%), Space: 56.5 MB (34.68%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums.length == 1){
+            return nums[0];
+        }
+        
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
